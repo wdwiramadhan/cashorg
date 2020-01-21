c4609563275dea6af5b1643da5db313036d7024c
wdwiramadhan <wahyu_dr71@yahoo.com>
2020-01-21 16:30:04 +0700

    fix

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,10 +13,10 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Admin Admin',
-            'email' => 'admin@argon.com',
+            'name' => 'wahyu',
+            'email' => 'admin@email.com',
             'email_verified_at' => now(),
-            'password' => Hash::make('secret'),
+            'password' => Hash::make('admin123'),
             'created_at' => now(),
             'updated_at' => now()
         ]);
